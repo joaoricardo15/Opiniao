c73089aca48b9449ae28c782a7b9b73ff4dc9c3a
joaoricardo15 <joaoricardo15@hotmail.com>
2017-11-29 01:16:00 -0200

    final4

--- www/js/index.js
+++ www/js/index.js
@@ -4,38 +4,86 @@ function getResults() {
   }
   else {
 
-    let city,sound,sport,party,activity,spirit;
+    let city,sound,tip,party,spirit;
+
+    let options = {
+      useEasing: true, 
+      useGrouping: true, 
+      separator: '', 
+      decimal: '.', 
+    };
 
     let myName = $("#my_name").val().toLowerCase();
     let someoneelseName = $("#someoneelse_name").val().toLowerCase();
 
     if( ( myName == "fernanda" && someoneelseName == "joao" ) || ( myName == "joao" && someoneelseName == "fernanda" ) ) {
 
-      city = "Curitiba";
-      sound = "Caetano Veloso";
-      sport = "bike";
-      party = "terraço secreto";
-      activity = "sexo gostoso";
-      spirit = "tranquilo & favorável";
+      var result = new CountUp('number', 0, 99, 0, 3, options);
 
-      $("#city").html("cidade mais apropriada: "+city);
-      $("#sound").html("som pra dividir no fone de ouvido: "+sound);
-      $("#sport").html("esporte se tiver com a disposição: "+sport);
-      $("#party").html("rolê de domingo: "+party);
-      $("#activity").html("coisa pra fazer bastante: "+activity);
-      $("#spirit").html("estado de espírito: "+spirit);
+      result.start(function() {
 
-    }
-    else if ( myName == "fernanda" || myName == "ana" || myName == "juliana" || myName == "joao" || myName == "paulo" || myName == "pedro" ) {
-      
-      $("#malsae").html( "O casal tem potencial... </br></br>A sugestão é muito sexo, muita água, não dirijam depois de beber que não tem erro!!!" );
+        setTimeout(function () {
+
+          result = new CountUp('number', 99, 99999, 0, 2, options);
+
+          result.start(function() {
+
+            $("#number").html("overflow");
+            
+            $("#message").html("</br><i class='fa fa-fire fa-2x' aria-hidden='true'></i> &nbsp&nbsp UaU, vai transar demais!!!</br></br>");
+            
+            setTimeout(function () {
+            
+              city = "Curitiba";
+              sound = "Caetano Veloso";
+              tip = "beber bastante água";
+              spirit = "tranquilo e favorável";
+              party = "terraço secreto";
+        
+              $("#city").html("</br>cidade mais apropriada: "+city);
+              $("#sound").html("trilha sonora sugerida: "+sound);
+              $("#tip").html("dica da sabedoria ancestral: "+tip);
+              $("#spirit").html("previsão do tempo: "+spirit);
+              $("#party").html("rolê de domingo: "+party+"</br></br>");
+
+            }, 30);//3000);  
+          
+          });
+
+        }, 50);
+
+      });
+
+       
 
     }
     else {
+    
+      var alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j","k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v","w", "x", "y", "z"];
 
-      $("#malsae").html( "mals ae!!! nosso algoritmo não foi capaz de avaliar essa combinação" );
+      let myNameIndex = alphabet.indexOf(myName[0])+1;
+      let someoneelseNameIndex = alphabet.indexOf(someoneelseName[0])+1;
 
-    } 
+      if ( myNameIndex > 0 && someoneelseNameIndex > 0 ) {
+
+        let i = (myNameIndex + myNameIndex) * 2;
+
+        let result = new CountUp('number', 0, i, 0, 3, options);
+        
+        result.start(function() {
+                    
+          $("#message").html("</br>Sacanagem moderada</br></br>");
+                  
+        });
+
+      }
+      else {
+
+        $("#malsae").html( "mals ae!!! nosso algoritmo não foi capaz de avaliar essa combinação" );
+
+      }     
+    
+    }
     
     $("#resultModal").modal("show");
 
@@ -45,7 +93,7 @@ function getResults() {
 window.onload = function() {
   
   $(document).ready(function(){
-    
+
     $("#resultModal").on('show.bs.modal', function () {
 
       $(".modal-title").html( "resultados para " + $("#my_name").val() + " & " + $("#someoneelse_name").val() );
@@ -56,12 +104,13 @@ window.onload = function() {
       $("#my_name").val("");
       $("#someoneelse_name").val("");
 
+      $("#number").html("");
+      $("#message").html("");
       $("#city").html("");
       $("#sound").html("");
-      $("#sport").html("");
-      $("#party").html("");
-      $("#activity").html("");
+      $("#tip").html("");
       $("#spirit").html("");
+      $("#party").html("");
       $("#malsae").html("");
 
     });
